96f19e04c5fad0b2c78bb8a692131c0340d29a59
Jabez Dailey <jabezdailey@icloud.com>
2024-05-21 10:22:54 -0400

    added envelopes and transactions

--- src/pages/Budget/Budget.jsx
+++ src/pages/Budget/Budget.jsx
@@ -3,10 +3,90 @@ import styles from "./Budget.module.css";
 
 const Budget = ({ }) => {
   return (
-    <div>
-      <h1>Envelopes</h1>
-      <h1>Transactions</h1>
-    </div>
+    <main>
+      <div className={styles.section}>
+        <h1 className={styles.sectionHeader}>Envelopes</h1>
+        <div className={styles.envelopes}>
+          <div className={styles.envelope}>
+            <h2>Needs</h2>
+            <p>$100 out of $250 spent</p>
+          </div>
+          <div className={styles.envelope}>
+            <h2>Wants</h2>
+            <p>$100 out of $250 spent</p>
+          </div>
+          <div className={styles.envelope}>
+            <h2>Savings</h2>
+            <p>$100 out of $250 spent</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <h1 className={styles.sectionHeader}>Transactions</h1>
+        <div className={styles.transactions}>
+          <div className={styles.transaction}>
+            <div className={styles.transactionLeft}>
+              <h2>Gas Station</h2>
+              <p>Needs</p>
+            </div>
+            <div className={styles.transactionRight}>
+              <h2>-$20.00</h2>
+              <p>Mar. 4th</p>
+            </div>
+          </div>
+          <div className={styles.transaction}>
+            <div className={styles.transactionLeft}>
+              <h2>Gas Station</h2>
+              <p>Needs</p>
+            </div>
+            <div className={styles.transactionRight}>
+              <h2>-$20.00</h2>
+              <p>Mar. 4th</p>
+            </div>
+          </div>
+          <div className={styles.transaction}>
+            <div className={styles.transactionLeft}>
+              <h2>Gas Station</h2>
+              <p>Needs</p>
+            </div>
+            <div className={styles.transactionRight}>
+              <h2>-$20.00</h2>
+              <p>Mar. 4th</p>
+            </div>
+          </div>
+          <div className={styles.transaction}>
+            <div className={styles.transactionLeft}>
+              <h2>Gas Station</h2>
+              <p>Needs</p>
+            </div>
+            <div className={styles.transactionRight}>
+              <h2>-$20.00</h2>
+              <p>Mar. 4th</p>
+            </div>
+          </div>
+          <div className={styles.transaction}>
+            <div className={styles.transactionLeft}>
+              <h2>Gas Station</h2>
+              <p>Needs</p>
+            </div>
+            <div className={styles.transactionRight}>
+              <h2>-$20.00</h2>
+              <p>Mar. 4th</p>
+            </div>
+          </div>
+          <div className={styles.transaction}>
+            <div className={styles.transactionLeft}>
+              <h2>Gas Station</h2>
+              <p>Needs</p>
+            </div>
+            <div className={styles.transactionRight}>
+              <h2>-$20.00</h2>
+              <p>Mar. 4th</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
