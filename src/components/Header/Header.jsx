de09bf7276d1a30389cf645fb2f4887b9543e70b
Jabez Dailey <jabezdailey@icloud.com>
2024-05-21 09:36:58 -0400

    created menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,41 +11,51 @@ const Header = ({ }) => {
   };
 
   return (
-    <header className={styles.header}>
-      <div className={styles.headerInfo}>
-        <div className={styles.userName}>
-          <h1>Hi, Jabez</h1>
-          <button className={styles.hamburgerIcon} onClick={toggleMenu}>
-            <img src={hamburgerIcon} alt="Menu" width={24} height={24} />
-          </button>
-        </div>
-        <div className={styles.totals}>
-          <div className={styles.total}>
-            <h2>Total Planned Expenses</h2>
-            <p>$1,000</p>
+    <div>
+      <header className={styles.header}>
+        <div className={styles.headerInfo}>
+          <div className={styles.userName}>
+            <h1>Hi, Jabez</h1>
+            <button className={styles.hamburgerIcon} onClick={toggleMenu}>
+              <img src={hamburgerIcon} alt="Menu" width={24} height={24} />
+            </button>
           </div>
-          <div className={styles.total}>
-            <h2>Total Remaining Expenses</h2>
-            <p>$1,000</p>
+          <div className={styles.totals}>
+            <div className={styles.total}>
+              <h2>Total Planned Expenses</h2>
+              <p>$1,000</p>
+            </div>
+            <div className={styles.total}>
+              <h2>Total Remaining Expenses</h2>
+              <p>$1,000</p>
+            </div>
           </div>
         </div>
-      </div>
-
-      {isOpen && (
-        <nav className={styles.mobileMenu}>
-          <button className={styles.closeMenuIcon} onClick={toggleMenu}>
-            <h2>x</h2>
-          </button>
-          <ul>
-            <li>Dashboard</li>
-            <li className={styles.active}><Link to="/">Budget</Link></li>
-            <li>Transactions</li>
-            <li>Reports</li>
-            <li>Settings</li>
-          </ul>
-        </nav>
-      )}
-    </header>
+        {isOpen && (
+          <nav className={styles.mobileMenu}>
+            <button className={styles.closeMenuIcon} onClick={toggleMenu}>
+              <h2>x</h2>
+            </button>
+            <ul>
+              <li>Dashboard</li>
+              <li className={styles.active}><Link to="/">Budget</Link></li>
+              <li>Transactions</li>
+              <li>Reports</li>
+              <li>Settings</li>
+            </ul>
+          </nav>
+        )}
+      </header>
+      <nav className={styles.menu}>
+        <ul>
+          <li>Dashboard</li>
+          <li className={styles.active}><Link to="/">Budget</Link></li>
+          <li>Transactions</li>
+          <li>Reports</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    </div>
 
   );
 
